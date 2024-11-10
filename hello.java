7a9b0a6efb2deb0bdf70a892ae409d409a2190c7
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2024-11-10 17:56:34 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class hello {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        
+        String name=sc.next();
+        System.out.println("My Default name is "+name);
+        sc.close();
+    }
+}
