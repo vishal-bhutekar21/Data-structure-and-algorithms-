1d744455800e14c1520f754e112ccf58242998c4
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2024-11-11 13:03:12 +0530

    Day one

---
+++ 02 Arrays/creationArray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class creationArray {
+  
+    public static void main(String[] args) {
+        int arr[]= {2,323,23,23,342,4123};
+
+        System.out.println(Arrays.toString(arr));
+
+
+         //string array 
+
+    String sArr[]={"s","423q4","535","Vishal"};
+
+    System.out.println(Arrays.toString(sArr));a 
+        
+    }
+
+   
+
+
+}
