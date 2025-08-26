ca3dc4f2aea1486b67d32bf61fe875128dc0fba8
Vishal Bhutekar <vishalbhutekar33772@gmail.com>
2025-08-26 13:36:06 +0530

    added new oop folder

---
+++ 06 OOP in java/ClassEx1.java
@@ -0,0 +1,14 @@
+public class ClassEx1 {
+   int age;
+   String name;
+
+}
+
+class ClassEx1Main{
+    public static void main(String s[]){
+        ClassEx1 e=new ClassEx1();
+        e.age=100;
+        System.out.println(e.age);
+        
+    }
+}
--- LeetcodeProblems/TwoSum.java
+++ LeetcodeProblems/TwoSum.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
@@ -14,7 +15,24 @@ public class TwoSum {
 
      static int[] twoSum(int[] arr, int target) {
 
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
 
+            int complement=target-arr[i];
+
+
+
+
+            if (map.containsKey(complement)) {
+
+                return new int[]{map.get(complement),i};
+                
+            }
+            
+            map.put(arr[i], i);
+
+        }
+        
 
 
         return new int[]{-1,-1};
